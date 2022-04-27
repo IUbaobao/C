503cc784583de276370a11ebeb1dc79b49da9075
IUbaobao <2868798970@qq.com>
2022-04-27 08:54:13 +0800

    更新

--- SList/SList/text.c
+++ SList/SList/text.c
@@ -54,9 +54,119 @@ void SListText2()
 	//SListPrint(list);
 
 }
+void menu()
+{
+	printf("************************************\n");
+	printf("    1.头插   2.头删\n");
+	printf("    3.尾插   4.尾删\n");
+	printf("    5.打印   6.插入\n");
+	printf("    7.查找   8.删除指定位置后一个\n");
+	printf("    -1.退出\n");
+	printf("************************************\n");
+	printf(" 请选择>:   \n");
 
+}
+
+void menuText()
+{
+	SLNode* list = NULL;
+	int input = 0;
+	SLDateType x1 = 0;
+	while (input != -1)
+	{
+		menu();
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			printf("请输入你要头插入的数，以-1为结束\n");
+			scanf("%d", &x1);
+			while (x1 != -1)
+			{
+				SListPushFront(&list, x1);
+				scanf("%d", &x1);
+			}
+			printf("头插成功\n");
+			break;
+		case 2:
+			SListPopBack(&list);
+			printf("头删成功\n");
+			break;
+		case 3:
+			printf("请输入你要尾插入的数，以-1为结束\n");
+			scanf("%d", &x1);
+			while (x1 != -1)
+			{
+				SListPushBack(&list, x1);
+				scanf("%d", &x1);
+			}
+			printf("尾插成功\n");
+			break;
+		case 4:
+			SListPopBack(&list);
+			printf("尾删成功\n");
+			break;
+		case 5:
+			SListPrint(list);
+			break;
+		case 6:
+			printf("在pos位置后面插入,请输入你在哪个位置后插入\n");
+				scanf("%d", &x1);
+				SLNode* pos = SListFind(list, x1);
+				if (pos != NULL)
+				{
+					printf("OK,请输入你要插入的数据\n");
+					scanf("%d", &x1);
+					SListInsertAfter(pos,x1);
+					printf("插入成功\n");
+				}
+				else
+				{
+					printf("无此数据\n");
+				}
+			break;
+		case 7:
+			printf("你要查找哪个数据(pos)>\n");
+			scanf("%d", &x1);
+			int i = 1;
+			pos=SListFind(list, x1);
+			if (pos == NULL)
+			{
+				printf("无此数据\n");
+			}
+			while (pos)
+			{
+				printf("第%d个pos节点：%p->%d\n", i++, pos, pos->data);
+				pos = SListFind(pos->next, x1);
+
+			}
+			break;
+		case 8:
+			printf("你要删除哪个位置后的数据>\n");
+			scanf("%d", &x1);
+			pos = SListFind(list, x1);
+			if (pos != NULL)
+			{
+				SListEraseAfter(pos);
+				printf("删除成功\n");
+			}
+			else
+			{
+				printf("无此数据\n");
+			}
+			break;
+		case -1:
+			printf("退出成功\n");
+			break;
+		default:
+			printf("无此选项，请重新输入\n");
+			break;
+		}
+	}
+}
 int main()
 {
 	//SListText1();
-	SListText2();
+	//SListText2();
+	menuText();
 }
