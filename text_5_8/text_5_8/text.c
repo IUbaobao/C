ff75226f5e68a50429b5b6bff3461f3840b54982
IUbaobao <2868798970@qq.com>
2022-05-11 17:40:05 +0800

    更新

--- text_5_8/text_5_8/text.c
+++ text_5_8/text_5_8/text.c
@@ -68,3 +68,96 @@
 //	return 0;
 //}
 //
+
+
+
+
+//
+//#include<stdio.h>
+//
+//int main()
+//{
+//    int a, b, c;
+//    while (scanf("%d %d %d", &a, &b, &c) != EOF)
+//    {
+//        if ((a + b > c) && (a + c > b) && (b + c > a))
+//        {
+//            if ((a == b) && (a == c) && (b == c))
+//            {
+//                printf("Equilateral triangle!\n");
+//            }
+//
+//            else if ((a == b) || (a == c) || (b == c))
+//            {
+//                printf("Isosceles triangle!\n");
+//            }
+//            else
+//            {
+//                printf("Ordinary triangle!\n");
+//            }
+//        }
+//        else
+//            printf("Not a triangle!");
+//    }
+//    return 0;
+//}
+
+//获得月份天数
+//#include<stdio.h>
+//
+//int main()
+//{
+//    int year = 0;
+//    int month = 0;
+//    while (scanf("%d %d", &year, &month) != EOF)
+//    {
+//        if (month == 4 || month == 6 || month == 9 || month == 11)
+//        {
+//            printf("30\n");
+//        }
+//        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+//        {
+//            printf("31\n");
+//        }
+//        if (month == 2)
+//        {
+//            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+//            {
+//                printf("29\n");
+//            }
+//            else
+//            {
+//                printf("28\n");
+//            }
+//        }
+//    }
+//
+//
+//}
+//X形图案
+
+#include<stdio.h>
+
+int main()
+{
+    int n = 0;
+
+    int i = 0;
+    int j = 0;
+    while (scanf("%d", &n) != EOF)
+    {
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if (j == i || j == n - i - 1)
+                    printf("*");
+                else
+                    printf(" ");
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
