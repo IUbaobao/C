147f7c2968fa9731180960ea297b854b7c1e9988
IUbaobao <2868798970@qq.com>
2022-05-12 22:56:56 +0800

    添加作业

--- text_5_8/text_5_8/text.c
+++ text_5_8/text_5_8/text.c
@@ -162,19 +162,153 @@
 //    return 0;
 //}
 
+//
+//
+//#include<stdio.h>
+//int add(int x, int y)
+//{
+//
+//	return x+y;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = add(a, b);
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+
+//
+////写一个函数打印arr数组的内容，不使用数组下标，使用指针。
+////arr是一个整形一维数组。
+//#include<stdio.h>
+//
+//void print(int* p, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr, sz);
+//	return 0;
+//}
+
+////
+////求Sn = a + aa + aaa + aaaa + aaaaa的前5项之和，其中a是一个数字，
+////例如：2 + 22 + 222 + 2222 + 22222
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int i = 0;
+//	int sum = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		sum += a;
+//		a = a * 10 + a;
+//	}
+//	printf("%d ", sum);
+//
+//	return 0;
+//}
+//
+////
+////
+////求出0～100000之间的所有“水仙花数”并输出。
+////“水仙花数”是指一个n位数，其各位数字的n次方之和确好等于该数本身，
+////如 : 153＝1 ^ 3＋5 ^ 3＋3 ^ 3，则153是一个“水仙花数”。
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	int sum = 0;
+//	for (i = 1; i <= 100000; i++)
+//	{
+//		int j = i;
+//		int n = 0;
+//		while (j )
+//		{
+//			n++;
+//			j = j / 10;
+//		}
+//		j = i;
+//		sum = 0;
+//		while (j)
+//		{
+//		 sum = sum + (int)pow(j % 10, n);
+//			j /= 10;
+//		}
+//		if (sum == i)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+////}
+//
+////打印菱形
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	int n = 0;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		for (j = 1; j <= n - i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j < 2 * i - 1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	for (i = 1; i <= n-1; i++)
+//	{
+//		for (j = 1; j <=i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j < 2 * (n-i) - 1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//输入描述:
+//输入一个字符串，可以有空格
+//输出描述:
+//输出逆序的字符串
 
 
 #include<stdio.h>
-int add(int x, int y)
-{
+#include<string.h>
+#include<stdlib.h>
 
-	return x+y;
-}
 int main()
 {
-	int a = 10;
-	int b = 20;
-	int c = add(a, b);
-	printf("%d\n", c);
-	return 0;
+    char arr[10000];
+    gets(arr);
+    int i = strlen(arr) - 1;
+    for (i; i >= 0; i--)
+    {
+        printf("%c", arr[i]);
+    }
+    return 0;
 }
