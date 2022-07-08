01581d3899994b67abd0b0e269f002354be00449
IUbaobao <2868798970@qq.com>
2022-07-08 21:54:09 +0800

    局部优先

---
+++ test_7_8/test_7_8/test.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include<stdio.h>
+//
+//int x = 19;
+//int y = 20;
+//void swap(int *x,int *y)
+//{
+//	int z;
+//	z = *x;
+//	*x = *y;
+//	*y =z;
+//}
+//int main()
+//{
+//	int x = 3;
+//	int y = 4;
+//	swap(&x,&y);
+//	printf("%d,%d", x, y);//局部优先
+//	return 0;
+//}
+
+#include<stdio.h>
+
+int main()
+{
+
+	return 0;
+}
