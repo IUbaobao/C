e0a715afffaca2d72eb064b312d1ad226cf19196
IUbaobao <2868798970@qq.com>
2022-07-11 21:11:10 +0800

    结构体大小（内存对齐）

---
+++ test_7_11/test_7_11/test.c
@@ -0,0 +1,104 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+//struct A
+//{
+//	int a;
+//	short b;
+//	int c;
+//	char d;
+//};
+//struct B
+//{
+//	int a;
+//	short b;
+//	char c;
+//	int d;
+//};
+//int main()
+//{
+//	printf("%d\n", sizeof(struct A));
+//	printf("%d\n", sizeof(struct B));
+//
+//	return 0;
+//}
+
+
+
+//#pragma pack(4)/*编译选项，表示4字节对齐 平台：VS2013。语言：C语言*/
+//int main(int argc, char* argv[])
+//{
+//    struct tagTest1
+//    {
+//        short a;
+//        char d;
+//        long b;
+//        long c;
+//    };
+//    struct tagTest2
+//    {
+//        long b;
+//        short c;
+//        char d;
+//        long a;
+//    };
+//    struct tagTest3
+//    {
+//        short c;
+//        long b;
+//        char d;
+//        long a;
+//    };
+//    struct tagTest1 stT1;
+//    struct tagTest2 stT2;
+//    struct tagTest3 stT3;
+//
+//    printf("%d %d %d", sizeof(stT1), sizeof(stT2), sizeof(stT3));
+//    return 0;
+//}
+//#pragma pack()
+//
+//#define MAX_SIZE 5
+//struct _Record_Struct
+//{
+//	unsigned char Env_Alarm_ID : 4;
+//	unsigned char Para1 : 2;
+//	unsigned char state;
+//	unsigned char avail : 1;
+//}*Env_Alarm_Record;
+//
+//int main()
+//{
+//	struct _Record_Struct* pointer = (struct _Record_Struct*)malloc
+//	(sizeof(struct _Record_Struct) * MAX_SIZE);
+//	printf("%d\n", sizeof(struct _Record_Struct));
+//	return 0;
+//}
+
+
+//int main()
+//{
+//    unsigned char puc[4];
+//    struct tagPIM
+//    {
+//        unsigned char ucPim1;
+//        unsigned char ucData0 : 1;
+//        unsigned char ucData1 : 2;
+//        unsigned char ucData2 : 3;
+//    }*pstPimData;
+//    typedef struct {
+//        int a;
+//        char b;
+//        short c;
+//        short d;
+//    }AA_t;
+//    pstPimData = (struct tagPIM*)puc;
+//    memset(puc, 0, 4);
+//    pstPimData->ucPim1 = 2;
+//    pstPimData->ucData0 = 3;
+//    pstPimData->ucData1 = 4;
+//    pstPimData->ucData2 = 5;
+//    printf("%02x %02x %02x %02x\n", puc[0], puc[1], puc[2], puc[3]);
+//    printf("%d\n", sizeof(AA_t));
+//    return 0;
+//}
