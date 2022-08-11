a1c150e38c50113c17f2acbba9f0228a7d225f9e
IUbaobao <2868798970@qq.com>
2022-08-11 21:09:19 +0800

    更新bug

--- test_8_11/test_8_11/test.c
+++ test_8_11/test_8_11/test.c
@@ -24,7 +24,7 @@
 
 
 
-//#include<stdio.h>
+#include<stdio.h>
 void Swap(int* e1, int* e2)
 {
 	int tem = *e1;
@@ -53,7 +53,7 @@ void AdjustDown(int* a, int n, int parent)
 		}
 	}
 }
-//
+////
 //void HeapSort(int* a, int n)
 //{
 //	//建堆
@@ -85,35 +85,14 @@ void AdjustDown(int* a, int n, int parent)
 
 #include<stdio.h>
 
-
-
-//void AdjustUp(int* a, int child)
-//{
-//	int parent = (child - 1) / 2;
-//	while (child > 0)
-//	{
-//		if (a[parent] < a[child])
-//		{
-//			Swap(&a[parent], &a[child]);
-//			child = parent;
-//			parent = (child - 1) / 2;
-//		}
-//		else
-//		{
-//			break;
-//		}
-//	}
-//}
-
 void AdjustUp(int* a, int child)
 {
 	int parent = (child - 1) / 2;
-	//while (parent >= 0)
 	while (child > 0)
 	{
-		if (a[child] > a[parent])
+		if (a[parent] < a[child])
 		{
-			Swap(&a[child], &a[parent]);
+			Swap(&a[parent], &a[child]);
 			child = parent;
 			parent = (child - 1) / 2;
 		}
@@ -124,17 +103,77 @@ void AdjustUp(int* a, int child)
 	}
 }
 
+
 int main()
 {
 	int arr[] = { 1,5,3,8,7,6 };
 	int n = sizeof(arr) / sizeof(arr[0]);
-	for (int i = n-1; i >=0;  --i)
+
+	for (int i = 0; i < n;  ++i)//建小堆
 	{
 		AdjustUp(arr, i);
 	}
-	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i)
+
+	//for (int i = (n - 1-1)/2; i >= 0; --i)//建小堆
+	//{
+	//	AdjustDown(arr,n, i);
+	//}
+
+	for (int i = 0; i < n; ++i)
 	{
 		printf("%d ", arr[i]);
 	}
 	return 0;
 }
+
+
+//#include<stdio.h>
+//
+//void Swap(int* e1, int* e2)
+//{
+//	int tem = *e1;
+//	*e1 = *e2;
+//	*e2 = tem;
+//}
+//
+////向下调整算法
+//void AdjustDown(int* a, int n, int parent)
+//{
+//	int midchild = parent * 2 + 1;
+//	while (midchild < n)
+//	{
+//		//建小堆找小的那个孩子，建大堆找的大的孩子
+//		if (midchild + 1 < n && a[midchild] < a[midchild + 1])
+//		{
+//			midchild++;
+//		}
+//		if (a[parent] < a[midchild])
+//		{
+//			Swap(&a[parent], &a[midchild]);//交换
+//			parent = midchild;
+//			midchild = parent * 2 + 1;
+//		}
+//		else
+//		{
+//			break;
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,5,3,8,7,6 };
+//	int n = sizeof(arr) / sizeof(arr[0]);
+//	//从最后一个非叶子节点开始调整，最后一个非叶子节点就是最后一个节点的父亲，
+//	//parent=(child-1)/2,  这里的最后一个元素为n-1，所以child=n-1，故parent=(n-1-1)/2
+//	for (int i = (n - 1-1)/2; i >= 0; --i)//建大堆
+//	{
+//		AdjustDown(arr,n, i);
+//	}
+//
+//	for (int i = 0; i <n; ++i)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
