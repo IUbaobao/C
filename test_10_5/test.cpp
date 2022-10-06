fb47e2fc79ee3c184d5838177729a0029989df7b
IUbaobao <2868798970@qq.com>
2022-10-06 15:36:41 +0800

    +运算符重载

--- test_10_5/test.cpp
+++ test_10_5/test.cpp
@@ -138,44 +138,259 @@ using namespace std;
 
 
 
+//
+//class Time
+//{
+//public:
+//	Time()
+//	{
+//		cout << "Time()" << endl;
+//		_hour = 0;
+//		_minute = 0;
+//		_second = 0;
+//	}
+//private:
+//	int _hour;
+//	int _minute;
+//	int _second;
+//};
+//class Date
+//{
+//public:
+//	void Print()
+//	{
+//		cout << _year << "-" << _month<<"-"<<_day << endl;
+//	}
+//private:
+//	// 基本类型(内置类型)
+//	int _year=0;//注意这只是给默认值，并没有开辟空间，跟函数上的缺省可以类比
+//	int _month=0;
+//	int _day=0;
+//	//// 自定义类型
+//	//Time _t;
+//};
+//int main()
+//{
+//	Date d;
+//	d.Print();
+//	return 0;
+//	
+//}
 
-class Time
-{
-public:
-	Time()
-	{
-		cout << "Time()" << endl;
-		_hour = 0;
-		_minute = 0;
-		_second = 0;
-	}
-private:
-	int _hour;
-	int _minute;
-	int _second;
-};
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b,c;
+//	char f;//接收+-*/
+//	printf("请输入第一个数\n");
+//	scanf("%d", &a);
+//
+//	printf("请输入第二个数\n");
+//	scanf("%d", &b);
+//
+//	getchar();//清空缓存中的'\n'
+//
+//	printf("请输入执行的法则:+-*/ :");
+//	scanf("%c", &f); //接收char类型的字符需要%c
+//
+//		switch (f)
+//		{
+//		case '+':
+//			c = a + b;
+//			break;
+//		case '-':
+//			c = a - b;
+//			break;
+//
+//		case '*':
+//			c = a * b;
+//			break;
+//
+//		case '/':
+//			c = a / b;
+//			break;
+//
+//		default:
+//			printf("无效法则\n");
+//			return 0;
+//		}
+//	printf("结果为%d\n", c);
+//	return 0;
+//}
+//
+
+
+//typedef int DataType;
+//class Stack
+//{
+//public:
+//	Stack(size_t capacity = 3)
+//	{
+//		_array = (DataType*)malloc(sizeof(DataType) * capacity);
+//		if (NULL == _array)
+//		{
+//			perror("malloc申请空间失败!!!");
+//			return;
+//		}
+//		_capacity = capacity;
+//		_size = 0;
+//	}
+//	void Push(DataType data)
+//	{
+//		// CheckCapacity();
+//		_array[_size] = data;
+//		_size++;
+//	}
+//	// 其他方法...
+//	~Stack()
+//	{
+//		if (_array)
+//		{
+//			free(_array);
+//			_array = NULL;
+//			_capacity = 0;
+//			_size = 0;//		}
+//	}
+//private:
+//	DataType* _array;
+//	int _capacity;
+//	int _size;
+//};
+//int main()
+//{
+//	Stack s;
+//	s.Push(1);
+//	s.Push(2);
+//	return 0;
+//}
+
+//
+//class Time
+//{
+//public:
+//	~Time()
+//	{
+//		cout << "~Time()" << endl;
+//	}
+//private:
+//	int _hour;
+//	int _minute;
+//	int _second;
+//};
+//class Date
+//{
+//private:
+//	// 基本类型(内置类型)
+//	int _year = 1970;
+//	int _month = 1;
+//	int _day = 1;
+//	// 自定义类型
+//	Time _t;
+//};
+//int main()
+//{
+//	Date d;
+//	return 0;
+//}
+// 程序运行结束后输出：~Time()
+// 在main方法中根本没有直接创建Time类的对象，为什么最后会调用Time类的析构函数？
+// 因为：main方法中创建了Date对象d，而d中包含4个成员变量，其中_year, _month, _day三个是
+// 内置类型成员，销毁时不需要资源清理，最后系统直接将其内存回收即可；而_t是Time类对象，所以在
+// d销毁时，要将其内部包含的Time类的_t对象销毁，所以要调用Time类的析构函数。但是：main函数
+// 中不能直接调用Time类的析构函数，实际要释放的是Date类对象，所以编译器会调用Date类的析构函
+// 数，而Date没有显式提供，则编译器会给Date类生成一个默认的析构函数，目的是在其内部调用Time
+// 类的析构函数，即当Date对象销毁时，要保证其内部每个自定义对象都可以正确销毁
+// main函数中并没有直接调用Time类析构函数，而是显式调用编译器为Date类生成的默认析构函数
+// 注意：创建哪个类的对象则调用该类的析构函数，销毁那个类的对象则调用该类的析构函数
+
+
+
 class Date
 {
 public:
+	Date(int year=1,int month=1,int day=1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
 	void Print()
 	{
-		cout << _year << "-" << _month<<"-"<<_day << endl;
+		cout << _year << "-" << _month << "-" << _day << endl;
 	}
+
+	bool operator==(const Date& d1)
+	{
+		return _year == d1._year
+			&& _month == d1._month
+			&& _day == d1._day;
+	}
+	
+	bool operator>(const Date& d1)
+	{
+		if (_year > d1._year)
+		{
+			return true;
+		}
+		else if (_year == d1._year && _month > d1._month)
+		{
+			return true;
+		}
+		else if (_year == d1._year && _month == d1._month && _day > d1._day)
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	bool operator>=(const Date& d1)
+	{
+		return *this > d1
+			|| *this == d1;
+	}
+	int GetMonthDay(int year, int month)
+	{
+		static int MonthDayArr[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+		if ( month==2 &&((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
+		{
+			return 29;
+		}
+		return MonthDayArr[month];
+	}
+
+	Date& operator+=(int day)
+	{
+		_day += day;
+		while (_day > GetMonthDay(_year,_month))
+		{
+			_day -= GetMonthDay(_year, _month);
+			_month++;
+			if (_month == 13)
+			{
+				_year++;
+				_month = 1;
+			}
+		}
+		return *this;
+	}
+	
 private:
-	// 基本类型(内置类型)
-	int _year=0;//注意这只是给默认值，并没有开辟空间，跟函数上的缺省可以类比
-	int _month=0;
-	int _day=0;
-	//// 自定义类型
-	//Time _t;
+	int _year;
+	int _month;
+	int _day;
 };
+
 int main()
 {
-	Date d;
-	d.Print();
+	Date d1(2022,9,22);
+//	Date d2(2022, 10, 7);
+
+	//cout << (d1 == d2) << endl;
+//	cout << (d1 >= d2) << endl;
+
+	d1 += 50;
+
+
 	return 0;
-	
 }
-
-
-
