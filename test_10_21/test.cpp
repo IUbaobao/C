f57100f25e09d0462ceca78f2c74e7c3f1c69553
IUbaobao <2868798970@qq.com>
2022-10-21 20:17:40 +0800

    类和对象下（写笔记用）

---
+++ test_10_21/test.cpp
@@ -0,0 +1,305 @@
+#include<iostream>
+using namespace std;
+
+
+//class Date
+//{
+//public:
+//	////传统方式初始化
+//	//Date(int year, int month, int day)
+//	//{
+//	//	_year = year;
+//	//	_month = month;
+//	//	_day = day;
+//	//}
+//
+//	//初始化列表方式初始化
+//	Date(int year, int month, int day)
+//		: _year(year)
+//		, _month(month)
+//		, _day(day)
+//	{}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+
+//class Date
+//{
+//public:
+//	// 1. 单参构造函数，没有使用explicit修饰，具有类型转换作用
+//	// explicit修饰构造函数，禁止类型转换---explicit去掉之后，代码可以通过编译
+//	 Date(int year)
+//		:_year(year)
+//	{}
+//	
+//	Date& operator=(const Date& d)
+//	{
+//		if (this != &d)
+//		{
+//			_year = d._year;
+//			_month = d._month;
+//			_day = d._day;
+//		}
+//		return *this;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//int main()
+//{
+//	Date d1(2022);
+//	// 用一个整形变量给日期类型对象赋值
+//	// 实际编译器背后会用2023构造一个无名对象，最后用无名对象给d1对象进行赋值
+//	d1 = 2023;
+//	// 将1加上explicit编译就会失败，因为explicit修饰构造函数，禁止了单参构造函数类型转
+//	//换的作用
+//	return 0;
+//}
+
+
+//class Date
+//{
+//public:
+//	Date(int year, int month, int day)
+//		: _year(year)
+//		, _month(month)
+//		, _day(day)
+//	{}
+//	// d1 << cout; -> d1.operator<<(&d1, cout); 不符合常规调用
+//	// 因为成员函数第一个参数一定是隐藏的this，所以d1必须放在<<的左侧
+//	ostream& operator<<(ostream& _cout)
+//	{
+//		_cout << _year << "-" << _month << "-" << _day << endl;
+//		return _cout;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+
+//
+//class Date
+//{
+//	//友元声明（可在类中的任意位置声明，不包括成员函数体内）
+//	friend ostream& operator<<(ostream& _cout, const Date& d);
+//	friend istream& operator>>(istream& _cin, Date& d);
+//public:
+//	Date(int year = 1970, int month = 1, int day = 1)
+//		: _year(year)
+//		, _month(month)
+//		, _day(day)
+//	{}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//
+//};
+//ostream& operator<<(ostream& _cout, const Date& d)
+//{
+//	_cout << d._year << "-" << d._month << "-" << d._day;
+//	return _cout;
+//}
+//istream& operator>>(istream& _cin, Date& d)
+//{
+//	_cin >> d._year;
+//	_cin >> d._month;
+//	_cin >> d._day;
+//	return _cin;
+//}
+//int main()
+//{
+//	Date d;
+//	cin >> d;
+//	cout << d << endl;
+//	return 0;
+//}
+
+
+//class Time
+//{
+//	friend class Date; // 声明日期类为时间类的友元类，则在日期类中就直接访问Time类中的私有成员变量
+//public:
+//	Time(int hour = 0, int minute = 0, int second = 0)
+//		: _hour(hour)
+//		, _minute(minute)
+//		, _second(second)
+//	{}
+//private:
+//	int _hour;
+//	int _minute;
+//	int _second;
+//};
+//class Date
+//{
+//public:
+//	Date(int year = 1900, int month = 1, int day = 1)
+//		: _year(year)
+//		, _month(month)
+//		, _day(day)
+//	{}
+//	void SetTimeOfDate(int hour, int minute, int second)
+//	{
+//		// 直接访问时间类私有的成员变量
+//		_t._hour = hour;
+//		_t._minute = minute;
+//		_t._second = second;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//	Time _t;
+//};
+
+
+//class stu
+//{
+//public:
+//	class cat
+//	{
+//	private:
+//		int age;
+//		char name[10];
+//	};
+//private:
+//	int age;
+//	char name[10];
+//};
+//
+//int main()
+//{
+//	cout << sizeof(stu) << endl;
+//	cout << sizeof(stu::cat) << endl;
+//	return 0;
+//}
+//class A
+//{
+//public:
+//	A(int a = 0)
+//		:_a(a)
+//	{
+//		cout << "A(int a)" << endl;
+//	}
+//	~A()
+//	{
+//		cout << "~A()" << endl;
+//	}
+//private:
+//	int _a;
+//};
+//class Solution {
+//public:
+//	int Sum_Solution(int n) {
+//		//...
+//		return n;
+//	}
+//};
+//int main()
+//{
+//	A aa1;
+//	// 不能这么定义对象，因为编译器无法识别下面是一个函数声明，还是对象定义
+//	//A aa1();
+//	// 但是我们可以这么定义匿名对象，匿名对象的特点不用取名字，
+//	// 但是他的生命周期只有这一行，我们可以看到下一行他就会自动调用析构函数
+//	A();
+//	A aa2(2);
+//	// 匿名对象在这样场景下就很好用，当然还有一些其他使用场景
+//	Solution().Sum_Solution(10);
+//	return 0;
+//}
+
+
+//class Data
+//{
+//public:
+//	Data(int year=1970,int month=0,int day=0)
+//		:_year(year)
+//		, _month(month)
+//		, _day(day)
+//	{
+//		cout << "Data()" << endl;
+//	}
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//int main()
+//{
+//	Data d1 = 1;
+//	Data d2 = { 2022, 10, 21 };
+//	return 0;
+//}
+
+
+class A
+{
+public:
+	A(int a = 0)
+		:_a(a)
+	{
+		cout << "A(int a)" << endl;
+	}
+	A(const A& aa)
+		:_a(aa._a)
+	{
+		cout << "A(const A& aa)" << endl;
+	}
+	A& operator=(const A& aa)
+	{
+		cout << "A& operator=(const A& aa)" << endl;
+		if (this != &aa)
+		{
+			_a = aa._a;
+		}
+		return *this;
+	}
+	~A()
+	{
+		cout << "~A()" << endl;
+	}
+private:
+	int _a;
+};
+void f1(A aa)
+{}
+A f2()
+{
+	A aa;
+	return aa;
+}
+int main()
+{
+	A a1 = 1; //A tem(1) + A a1(tem) -> 优化 A a1(1)
+	cout << endl;
+
+	// 传值传参
+	A aa1;
+	f1(aa1);//f1中的临时aa拷贝构造aa1，而且具有常属性const
+	cout << endl;
+
+	// 传值返回
+	f2();//同理，f2中的aa是传值返回，也要拷贝构造一个临时对象返回，也具有常属性const
+	cout << endl;
+
+	// 一个表达式中，连续拷贝构造+拷贝构造->优化一个拷贝构造
+	A ret = f2();
+	//下面这种情况就无法优化了
+/*	A ret;
+	ret = f2()*/;
+	cout << endl;
+
+	// 一个表达式中，连续拷贝构造+赋值重载->无法优化
+	aa1 = f2();
+	cout << endl;
+	return 0;
+}
