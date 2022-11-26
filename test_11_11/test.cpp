9f41f64edf633e1d4d679a2f054ff88a9d95389d
IUbaobao <2868798970@qq.com>
2022-11-26 22:18:45 +0800

    string的博客代码

--- test_11_11/test.cpp
+++ test_11_11/test.cpp
@@ -639,40 +639,243 @@
 //	return 0;
 //}
 
+//
+//
+//#include<iostream>
+//#include<algorithm>
+//#include<vector>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	vector<int> v(n);
+//	for (int i = 0; i < n; ++i)
+//	{
+//		cin >> v[i];
+//	}
+//	int sum = 0,maxsum=0,begin=0;
+//	for (int i = 0; i < v.size(); ++i)
+//	{
+//		sum += v[i];
+//		if (sum > maxsum)
+//		{
+//			while (i<v.size() && sum + v[i]>maxsum)
+//			{
+//				sum += v[i++];
+//			}
+//			maxsum = sum;
+//		}
+//		while (begin<=i && sum - v[begin]>maxsum )
+//		{
+//			sum -= v[begin++];
+//		}
+//
+//		
+//	}
+//	cout << maxsum << endl;
+//	return 0;
+//}
+//
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() 
+//{
+//	//string构造
+//	string s1; //创建空字符串，调用无参构造函数
+//	cout << "str1 = " << s1 << endl;
+//
+//	const char* str = "hello world";
+//	string s2(str); //把c_string转换成了string
+//
+//	cout << "str2 = " << s2 << endl;
+//
+//	string s3(s2); //调用拷贝构造函数
+//	cout << "str3 = " << s3 << endl;
+//
+//	string s4(10, 'a');
+//	cout << "str3 = " << s3 << endl;
+//	return 0;
+//}
 
 
-#include<iostream>
-#include<algorithm>
-#include<vector>
+#include <iostream>
+#include <string>
 using namespace std;
+//int main() {
+//	//赋值
+//	string str1;
+//	str1 = "hello world";
+//	cout << "str1 = " << str1 << endl;
+//
+//	string str2;
+//	str2 = str1;
+//	cout << "str2 = " << str2 << endl;
+//
+//	string str3;
+//	str3 = 'a';
+//	cout << "str3 = " << str3 << endl;
+//
+//	string str4;
+//	str4.assign("hello c++");
+//	cout << "str4 = " << str4 << endl;
+//
+//	string str5;
+//	str5.assign("hello c++", 5);
+//	cout << "str5 = " << str5 << endl;
+//
+//
+//	string str6;
+//	str6.assign(str5);
+//	cout << "str6 = " << str6 << endl;
+//
+//	string str7;
+//	str7.assign(5, 'x');
+//	cout << "str7 = " << str7 << endl;
+//
+//	return 0;
+//}
+
+//
+//int main() {
+//	//字符串拼接
+//	string str1 = "我";
+//
+//	str1 += "爱玩游戏";
+//
+//	cout << "str1 = " << str1 << endl;
+//
+//	str1 += ':';
+//
+//	cout << "str1 = " << str1 << endl;
+//
+//	string str2 = "Minecraft";
+//
+//	str1 += str2;
+//
+//	cout << "str1 = " << str1 << endl;
+//
+//	string str3 = "I";
+//	str3.append(" love ");
+//	str3.append("game abcde", 4);
+//	//str3.append(str2);
+//	str3.append(str2, 4, 3); // 从下标4位置开始 ，截取3个字符，拼接到字符串末尾
+//	cout << "str3 = " << str3 << endl;
+//
+//	return 0;
+//}
+
+
+//查找和替换
+void test01()
+{
+	//查找
+	string str1 = "abcdefgde";
+
+	int pos = str1.find("de");
+
+	if (pos == -1)
+	{
+		cout << "未找到" << endl;
+	}
+	else
+	{
+		cout << "pos = " << pos << endl;
+	}
+
+
+	pos = str1.rfind("de");
+
+	cout << "pos = " << pos << endl;
+
+}
+
+void test02()
+{
+	//替换
+	string str1 = "abcdefgde";
+	str1.replace(1, 3, "1111");
+
+	cout << "str1 = " << str1 << endl;
+}
+
+
+void test03()
+{
+	string str = "hello world";
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		cout << str[i] << " ";
+	}
+	cout << endl;
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		cout << str.at(i) << " ";
+	}
+	cout << endl;
+
+
+	//字符修改
+	str[0] = 'x';
+	str.at(1) = 'x';
+	cout << str << endl;
+
+}
+
+//
+//int main() {
+//
+//	test03();
+//	return 0;
+//}
+
+//字符串插入和删除
+//int main() {
+//	string str = "hello";
+//	str.insert(1, "111");
+//	cout << str << endl;
+//
+//	str.erase(1, 3);  //从1号位置开始3个字符
+//	cout << str << endl;
+//	return 0;
+//}
+
+//
+////子串
+//int main() {
+//
+//	string str = "abcdefg";
+//	string subStr = str.substr(1, 3);
+//	cout << "subStr = " << subStr << endl;
+//
+//	string email = "hello@sina.com";
+//	int pos = email.find("@");
+//	string username = email.substr(0, pos);
+//	cout << "username: " << username << endl;
+//
+//	return 0;
+//}
+
+
+#include<string>
+#include"string.h"
+
 int main()
 {
-	int n;
-	cin >> n;
-	vector<int> v(n);
-	for (int i = 0; i < n; ++i)
-	{
-		cin >> v[i];
-	}
-	int sum = 0,maxsum=0,begin=0;
-	for (int i = 0; i < v.size(); ++i)
-	{
-		sum += v[i];
-		if (sum > maxsum)
-		{
-			while (i<v.size() && sum + v[i]>maxsum)
-			{
-				sum += v[i++];
-			}
-			maxsum = sum;
-		}
-		while (begin<=i && sum - v[begin]>maxsum )
-		{
-			sum -= v[begin++];
-		}
+	hdm::stringtest2();
+	//string s1("hello world");
 
-		
-	}
-	cout << maxsum << endl;
+	//cout << s1.c_str() << endl;
+	//size_t pos = s1.find('d');
+	//if (pos != -1)
+	//{
+	//	s1.insert(pos,1,'u');
+	//}
+	//cout << s1.c_str() << endl;
+
 	return 0;
 }
