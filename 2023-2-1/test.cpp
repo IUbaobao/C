a17d342be966f4dff0378059107419b28471f20b
IUbaobao <2868798970@qq.com>
2023-02-06 20:52:16 +0800

    继承

--- 2023-2-1/test.cpp
+++ 2023-2-1/test.cpp
@@ -377,44 +377,448 @@ public:
 //	return 0;
 //}
 
+//
+//#include "BSTress.h"
+//
+//int main()
+//{
+//	BSTress<int> t;
+//	int a[] = { 8, 3, 1, 10, 6, 4, 7, 14, 13 };
+//	for (auto e : a)
+//	{
+//		t.insert(e);
+//	}
+//	t.InOrder();
+//	cout << endl;
+//	//auto f = t.Find(13);
+//	//if (f != nullptr)
+//	//{
+//	//	cout << f->_val << endl;
+//	//}
+//	BSTress<int> t1(t);
+//	t1.InOrder();
+//	cout << endl;
+//	BSTress<int> t2;
+//	t2 = t1;
+//	t2.InOrder();
+//	cout << endl;
+//
+//	for (auto e : a)
+//	{
+//		t.eraseR(e);
+//		t.InOrder();
+//		cout << endl;
+//	}
+//	//t.erase(8);
+//	//t.InOrder();
+//	//cout << endl;
+//
+//	//t.erase(14);
+//	//t.InOrder();
+//	//cout << endl;
+//	return 0;
+//}
 
-#include "BSTress.h"
 
+//class A
+//{
+//public:
+//	int _a;
+//};
+//// class B : public A
+//class B :  virtual public A
+//{
+//public:
+//	int _b;
+//};
+//// class C : public A
+//class C : virtual public A
+//{
+//public:
+//	int _c;
+//};
+//class D : public B, public C
+//{
+//public:
+//	int _d;
+//};
+//int main()
+//{
+//	D d;
+//	d.B::_a = 1;
+//	d.C::_a = 2;
+//	d._b = 3;
+//	d._c = 4;
+//	d._d = 5;
+//	d._a = 6;
+//	cout << sizeof(D) << endl;
+//	return 0;
+//}
+
+//class Person
+//{
+//public:
+//	void Print()
+//	{
+//		cout << "name:" << _name << endl;
+//		cout << "age:" << _age << endl;
+//	}
+//protected:
+//	string _name = "peter"; // 姓名
+//	int _age = 18; // 年龄
+//};
+//// 继承后父类的Person的成员（成员函数+成员变量）都会变成子类的一部分。这里体现出了
+////Student和Teacher复用了Person的成员。下面我们使用监视窗口查看Student和Teacher对象，可
+////以看到变量的复用。调用Print可以看到成员函数的复用。
+//class Student : public Person
+//{
+//protected:
+//	int _stuid; // 学号
+//};
+//
+//class Teacher : public Person
+//{
+//protected:
+//	int _jobid; // 工号
+//};
+
+
+//class Person
+//{
+//protected:
+//	string _name; // 姓名
+//	string _sex; // 性别
+//	int _age; // 年龄
+//};
+//class Student : public Person
+//{
+//public:
+//	int _No; // 学号
+//};
+//void Test()
+//{
+//	Student sobj;
+//	// 1.子类对象可以赋值给父类对象/指针/引用
+//	Person pobj = sobj;
+//	Person* pp = &sobj;
+//	Person& rp = sobj;
+//	//2.基类对象不能赋值给派生类对象
+//	sobj = pobj;
+//	// 3.基类的指针可以通过强制类型转换赋值给派生类的指针
+//	pp = &sobj;
+//	Student* ps1 = (Student*)pp; // 这种情况转换时可以的。
+//	ps1->_No = 10;
+//	pp = &pobj;
+//	Student* ps2 = (Student*)pp; // 这种情况转换时虽然可以，但是会存在越界访问的问题
+//	ps2->_No = 10;
+//}
+
+
+
+// Student的_num和Person的_num构成隐藏关系，可以看出这样代码虽然能跑，但是非常容易混淆
+//class Person
+//{
+//protected:
+//	string _name = "小李子"; // 姓名
+//	int _num = 111; // 身份证号
+//};
+//class Student : public Person
+//{
+//public:
+//	void Print()
+//	{
+//		cout << " 姓名:" << _name << endl;
+//		cout << " 身份证号:" << Person::_num << endl;
+//		cout << " 学号:" << _num << endl;
+//	}
+//protected:
+//	int _num = 999; // 学号
+//};
+//void Test()
+//{
+//	Student s1;
+//	s1.Print();
+//};
+// B中的fun和A中的fun不是构成重载，因为不是在同一作用域
+// B中的fun和A中的fun构成隐藏，成员函数满足函数名相同就构成隐藏。
+//class A
+//{
+//public:
+//	void fun()
+//	{
+//		cout << "func()" << endl;
+//	}
+//};
+//class B : public A
+//{
+//public:
+//	void fun(int i)
+//	{
+//		A::fun();
+//		cout << "func(int i)->" << i << endl;
+//	}
+//};
+//
+//int main()
+//{
+//	B b;
+//	b.fun(10);
+//	return 0;
+//}
+
+
+//class Person
+//{
+//public:
+//	Person(const char* name = "peter")
+//		: _name(name)
+//	{
+//		cout << "Person()" << endl;
+//	}
+//	Person(const Person& p)
+//		: _name(p._name)
+//	{
+//		cout << "Person(const Person& p)" << endl;
+//	}
+//	Person& operator=(const Person& p)
+//	{
+//		cout << "Person operator=(const Person& p)" << endl;
+//		if (this != &p)
+//			_name = p._name;
+//		return *this;
+//	}
+//	~Person()
+//	{
+//		cout << "~Person()" << endl;
+//	}
+//protected:
+//	string _name; // 姓名
+//};
+//class Student : public Person
+//{
+//public:
+//	Student(const char* name, int num)
+//		: Person(name)//派生类要显示调用基类构造函数
+//		, _num(num)
+//	{
+//		cout << "Student()" << endl;
+//	}
+//	Student(const Student& s)
+//		: Person(s)
+//		, _num(s._num)
+//	{
+//		cout << "Student(const Student& s)" << endl;
+//	}
+//	Student& operator = (const Student& s)
+//	{
+//		cout << "Student& operator= (const Student& s)" << endl;
+//		if (this != &s)
+//		{
+//			Person::operator =(s);//显示调用基类赋值重载
+//			_num = s._num;
+//		}
+//		return *this;
+//	}
+//	~Student()
+//	{
+//		cout << "~Student()" << endl;
+//	}
+//protected:
+//	int _num; //学号
+//};
+//int main()
+//{
+//	Student s1("jack", 18);
+//	Student s2(s1);
+//	Student s3("rose", 17);
+//	s1 = s3;
+//	return 0;
+//}
+
+
+//
+//class Student;
+//class Person
+//{
+//public:
+//	friend void Display(const Person& p, const Student& s);
+//protected:
+//	string _name; // 姓名
+//};
+//class Student : public Person
+//{
+//protected:
+//	int _stuNum; // 学号
+//};
+//void Display(const Person& p, const Student& s)
+//{
+//	cout << p._name << endl;
+//	cout << s._stuNum << endl;//运行错误，友元关系不能继承
+//}
+//void main()
+//{
+//	Person p;
+//	Student s;
+//	Display(p, s);
+//}
+
+
+//class Base {
+//public:
+//	static void func()
+//	{
+//		cout << "Base - static void func()" << endl;
+//	}
+//	static void func(int a)
+//	{
+//		cout << "Base - static void func(int a)" << endl;
+//	}
+//
+//	static int m_A;
+//};
+//
+//int Base::m_A = 100;
+//
+//class Son : public Base {
+//public:
+//	static void func()
+//	{
+//		cout << "Son - static void func()" << endl;
+//	}
+//	static int m_A;
+//};
+//
+//int Son::m_A = 200;
+//
+////同名成员属性
+//void test01()
+//{
+//	//通过对象访问
+//	cout << "通过对象访问： " << endl;
+//	Son s;
+//	cout << "Son  下 m_A = " << s.m_A << endl;
+//	cout << "Base 下 m_A = " << s.Base::m_A << endl;
+//
+//	//通过类名访问
+//	cout << "通过类名访问： " << endl;
+//	cout << "Son  下 m_A = " << Son::m_A << endl;
+//	cout << "Base 下 m_A = " << Son::Base::m_A << endl;
+//}
+//
+////同名成员函数
+//void test02()
+//{
+//	//通过对象访问
+//	cout << "通过对象访问： " << endl;
+//	Son s;
+//	s.func();
+//	s.Base::func();
+//
+//	cout << "通过类名访问： " << endl;
+//	Son::func();
+//	Son::Base::func();
+//	//出现同名，子类会隐藏掉父类中所有同名成员函数，需要加作作用域访问
+//	Son::Base::func(100);
+//}
+//int main() {
+//
+//	test01();
+//	test02();
+//	return 0;
+//}
+
+
+//class Person
+//{
+//public:
+//	string _name; // 姓名
+//};
+//class Student : public Person
+//{
+//protected:
+//	int _num; //学号
+//};
+//class Teacher : public Person
+//{
+//protected:
+//	int _id; // 职工编号
+//};
+//class Assistant : public Student, public Teacher
+//{
+//protected:
+//	string _majorCourse; // 主修课程
+//};
+//void Test()
+//{
+//	// 这样会有二义性无法明确知道访问的是哪一个
+//	Assistant a;
+//	a._name = "peter";
+//	// 需要显示指定访问哪个父类的成员可以解决二义性问题，但是数据冗余问题无法解决
+//	a.Student::_name = "xxx";
+//	a.Teacher::_name = "yyy";
+//}
+
+
+//class Person
+//{
+//public:
+//	string _name; // 姓名
+//};
+//class Student : virtual public Person
+//{
+//protected:
+//	int _num; //学号
+//};
+//class Teacher : virtual public Person
+//{
+//protected:
+//	int _id; // 职工编号
+//};
+//class Assistant : public Student, public Teacher
+//{
+//protected:
+//	string _majorCourse; // 主修课程
+//};
+//void Test()
+//{
+//	Assistant a;
+//	a._name = "peter";
+//}
+
+
+
+class A
+{
+public:
+	int _a;
+};
+//class B : public A
+class B : virtual public A
+{
+public:
+	int _b;
+};
+//class C : public A
+class C : virtual public A
+{
+public:
+	int _c;
+};
+class D : public B, public C
+{
+public:
+	int _d;
+};
 int main()
 {
-	BSTress<int> t;
-	int a[] = { 8, 3, 1, 10, 6, 4, 7, 14, 13 };
-	for (auto e : a)
-	{
-		t.insert(e);
-	}
-	t.InOrder();
-	cout << endl;
-	//auto f = t.Find(13);
-	//if (f != nullptr)
-	//{
-	//	cout << f->_val << endl;
-	//}
-	BSTress<int> t1(t);
-	t1.InOrder();
-	cout << endl;
-	BSTress<int> t2;
-	t2 = t1;
-	t2.InOrder();
-	cout << endl;
-
-	for (auto e : a)
-	{
-		t.eraseR(e);
-		t.InOrder();
-		cout << endl;
-	}
-	//t.erase(8);
-	//t.InOrder();
-	//cout << endl;
-
-	//t.erase(14);
-	//t.InOrder();
-	//cout << endl;
+	D d;
+	d.B::_a = 1;
+	d.C::_a = 2;
+	d._b = 3;
+	d._c = 4;
+	d._d = 5;
+	d._a = 6;
 	return 0;
 }
+
+
