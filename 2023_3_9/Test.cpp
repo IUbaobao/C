62ccebced3b14f471dcf81fc9bc189c12b5cdd0b
IUbaobao <2868798970@qq.com>
2023-03-19 17:56:39 +0800

    多态笔记代码(已完结)

--- 2023_3_9/Test.cpp
+++ 2023_3_9/Test.cpp
@@ -1451,33 +1451,66 @@ int repeatedNTimes1(vector<int>& nums) {
 //}
 
 
-#include<iostream>
-using namespace std;
-class A{
-public:
-	A(char *s) { cout << s << endl; }
-	~A(){}
-};
-class B :virtual public A
+//#include<iostream>
+//using namespace std;
+//class A{
+//public:
+//	A(char *s) { cout << s << endl; }
+//	~A(){}
+//};
+////class B :virtual public A
+//class B : public A
+//
+//{
+//public:
+//	B(char *s1, char*s2) :A(s1) { cout << s2 << endl; }
+//};
+////class C :virtual public A
+//class C :public A
+//{
+//public:
+//	C(char *s1, char*s2) :A(s1) { cout << s2 << endl; }
+//};
+//class D :public B, public C
+//{
+//public:
+//	D(char *s1, char *s2, char *s3, char *s4) :C(s1, s3),B(s1, s2)
+//	{
+//		cout << s4 << endl;
+//	}
+//};
+//int main() {
+//	D *p = new D("class A", "class B", "class C", "class D");
+//	delete p;
+//	return 0;
+//}
+
+
+//class Base1 { public: int _b1; };
+//class Base2 { public: int _b2; };
+//class Derive : public Base1, public Base2 { public: int _d; };
+//int main(){
+//	Derive d;
+//	Base1* p1 = &d;
+//	Base2* p2 = &d;
+//	Derive* p3 = &d;
+//	return 0;
+//}
+class A
 {
 public:
-	B(char *s1, char*s2) :A(s1) { cout << s2 << endl; }
+	void func(int val = 1){ std::cout << "A->" << val << std::endl; }
+	virtual void test(){ func(); }
 };
-class C :virtual public A
+class B : public A
 {
 public:
-	C(char *s1, char*s2) :A(s1) { cout << s2 << endl; }
+	void func(int val = 0){ std::cout << "B->" << val << std::endl; }
 };
-class D :public B, public C
+int main(int argc, char* argv[])
 {
-public:
-	D(char *s1, char *s2, char *s3, char *s4) :B(s1, s2), C(s1, s3), A(s1)
-	{
-		cout << s4 << endl;
-	}
-};
-int main() {
-	D *p = new D("class A", "class B", "class C", "class D");
-	delete p;
+	A*p = new B;
+	p->test();
 	return 0;
 }
+
